705aa9273b6769e09a4b142e1870cef1015269fb
Radha Krishna <rkavulru@gmail.com>
2017-04-30 00:24:44 +0530

    changed port

--- affairal_server.py
+++ affairal_server.py
@@ -125,5 +125,5 @@ def shutdown():
     return 'Server shutting down...'
 if __name__ == '__main__':
      app.debug = True
-     port = int(os.environ.get("PORT", 5000))
+     port = int(os.environ.get("PORT", 33507))
      app.run(host='0.0.0.0', port=port)
